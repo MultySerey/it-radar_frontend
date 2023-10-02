f084126897e3668c06a8ff38ee2047f89b5efe83
MultySerey <multyserey@gmail.com>
2023-10-02 17:41:09 +0500

    init

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <header class="shadow-sm bg-white">
+        <nav class="container mx-auto p-4 flex justify-between">
+            <ul class="flex gap-4">
+                <NuxtLink to="/">IT-радар</NuxtLink>
+                <li><span>Курсы</span></li>
+                <li><span>Поиск</span></li>
+            </ul>
+            <span>Личный кабинет</span>
+        </nav>
+    </header>
+    <p>home page</p>
+    <div class="mx-auto p-4 grid grid-cols-3 gap-4">
+        <div v-for="c in 10">
+            c
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped></style>
