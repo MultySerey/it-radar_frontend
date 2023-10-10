261b4b173c79277e285eb43bd79bb3eea9c43257
MultySerey <multyserey@gmail.com>
2023-10-10 14:28:42 +0500

    login page

--- pages/user_enter.vue
+++ pages/user_enter.vue
@@ -1,14 +1,19 @@
 <template>
-    <div class="flex justify-evenly">
+    <div class="bg-slate-50 flex justify-evenly">
         <div class="my-container">
             <h1>Зарегистрироваться</h1>
-            <NicerInput text="Электронная почта" placeholder="Ivan.Ivanov@example.com" />
-            <NicerInput text="Пароль" placeholder="********" />
+            <div class="flex-col-gap">
+                <NicerInput text="Электронная почта" placeholder="Ivan.Ivanov@example.com" />
+                <NicerInput text="Пароль" placeholder="********" />
+            </div>
+            <NicerButton text="Зарегистрироваться" color="blue" />
         </div>
         <div class="my-container">
             <h1>Войти</h1>
-            <NicerInput text="Электронная почта" placeholder="Ivan.Ivanov@example.com" />
-            <NicerInput text="Пароль" placeholder="********" />
+            <div class="flex-col-gap">
+                <NicerInput text="Электронная почта" placeholder="Ivan.Ivanov@example.com" />
+                <NicerInput text="Пароль" placeholder="********" />
+            </div>
             <NicerButton text="Войти" color="blue" />
         </div>
     </div>
@@ -17,7 +22,13 @@
 <script></script>
 
 <style scoped>
+h1{
+    @apply p-4
+}
+.flex-col-gap{
+    @apply flex flex-col gap-4
+}
 .my-container {
-    @apply container shadow-lg border w-1/3 my-8 p-4 text-center
+    @apply bg-white container shadow-lg border w-1/3 my-8 px-16 py-8 text-center aspect-auto flex-col-gap justify-around
 }
 </style>
