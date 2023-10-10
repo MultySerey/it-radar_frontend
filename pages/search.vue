d47fc010f4a68a7ab2fff6c2c5e7b14a997e1af8
MultySerey <multyserey@gmail.com>
2023-10-10 14:27:56 +0500

    search proto

--- pages/search.vue
+++ pages/search.vue
@@ -1,6 +1,10 @@
 <template>
-    <div class="text-5xl text-center">
-        Тут будет поиск
+    <div>
+        <p class="text-5xl text-center">Тут будет поиск</p>
+        <div class="flex gap-2 px-16 py-2">
+            <NicerInput class="grow" placeholder="Запрос"></NicerInput>
+            <NicerButton class="grow-0" text="Найти" color="blue"></NicerButton>
+        </div>
     </div>
 </template>
 
