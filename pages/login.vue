ddaf54d9fd7083edf40c92724d612bcfdb7da9b3
MultySerey <multyserey@gmail.com>
2023-11-28 12:51:40 +0500

    simplify structure

--- pages/login.vue
+++ pages/login.vue
@@ -1,27 +1,25 @@
 <template>
     <div class="bg-slate-50 flex justify-evenly">
-        <div class="my-container">
-            <form @submit.prevent="submitForm">
-                <div class="flex-col-gap">
-                    <div class="flex flex-col gap-2">
-                        <label for="email">Электронная почта</label>
-                        <input id="email" type="email" v-model="request.email" class="border-2 rounded-lg p-2"
-                            placeholder="Ivan.Ivanov@example.com">
-                    </div>
-                    <div class="flex flex-col gap-2">
-                        <label for="password">Пароль</label>
-                        <input id="password" type="password" v-model="request.password" class="border-2 rounded-lg p-2"
-                            placeholder="********">
-                    </div>
+        <form @submit.prevent="submitForm" class="my-container">
+            <div class="flex-col-gap">
+                <div class="flex flex-col gap-2">
+                    <label for="email">Электронная почта</label>
+                    <input id="email" type="email" v-model="request.email" class="border-2 rounded-lg p-2"
+                        placeholder="Ivan.Ivanov@example.com">
                 </div>
-                <div>
-                    <button type="submit"
-                        class="w-fit bg-transparent hover:bg-blue-500 active:bg-blue-800 font-bold py-2 px-4 rounded-lg text-blue-500 hover:text-white border-2 border-blue-500 hover:border-transparent mt-8">
-                        <span>Войти</span>
-                    </button>
+                <div class="flex flex-col gap-2">
+                    <label for="password">Пароль</label>
+                    <input id="password" type="password" v-model="request.password" class="border-2 rounded-lg p-2"
+                        placeholder="********">
                 </div>
-            </form>
-        </div>
+            </div>
+            <div>
+                <button type="submit"
+                    class="w-fit bg-transparent hover:bg-blue-500 active:bg-blue-800 font-bold py-2 px-4 rounded-lg text-blue-500 hover:text-white border-2 border-blue-500 hover:border-transparent mt-8">
+                    <span>Войти</span>
+                </button>
+            </div>
+        </form>
     </div>
 </template>
 
--- pages/signup.vue
+++ pages/signup.vue
@@ -1,37 +1,35 @@
 <template>
     <div class="bg-slate-50 flex justify-evenly">
-        <div class="my-container">
-            <form @submit.prevent="submitForm">
-                <div class="flex-col-gap">
-                    <div class="flex flex-col gap-2">
-                        <label for="firstName">Имя</label>
-                        <input id="firstName" type="firstName" v-model="request.firstName" class="border-2 rounded-lg p-2"
-                            placeholder="Иван">
-                    </div>
-                    <div class="flex flex-col gap-2">
-                        <label for="lastName">Фамилия</label>
-                        <input id="lastName" type="text" v-model="request.lastName" class="border-2 rounded-lg p-2"
-                            placeholder="Иванов">
-                    </div>
-                    <div class="flex flex-col gap-2">
-                        <label for="email">Электронная почта</label>
-                        <input id="email" type="email" v-model="request.email" class="border-2 rounded-lg p-2"
-                            placeholder="Ivan.Ivanov@example.com">
-                    </div>
-                    <div class="flex flex-col gap-2">
-                        <label for="password">Пароль</label>
-                        <input id="password" type="password" v-model="request.password" class="border-2 rounded-lg p-2"
-                            placeholder="********">
-                    </div>
+        <form @submit.prevent="submitForm" class="my-container">
+            <div class="flex-col-gap">
+                <div class="flex flex-col gap-2">
+                    <label for="firstName">Имя</label>
+                    <input id="firstName" type="firstName" v-model="request.firstName" class="border-2 rounded-lg p-2"
+                        placeholder="Иван">
                 </div>
-                <div>
-                    <button type="submit"
-                        class="w-fit bg-transparent hover:bg-blue-500 active:bg-blue-800 font-bold py-2 px-4 rounded-lg text-blue-500 hover:text-white border-2 border-blue-500 hover:border-transparent mt-8">
-                        <span>Войти</span>
-                    </button>
+                <div class="flex flex-col gap-2">
+                    <label for="lastName">Фамилия</label>
+                    <input id="lastName" type="text" v-model="request.lastName" class="border-2 rounded-lg p-2"
+                        placeholder="Иванов">
                 </div>
-            </form>
-        </div>
+                <div class="flex flex-col gap-2">
+                    <label for="email">Электронная почта</label>
+                    <input id="email" type="email" v-model="request.email" class="border-2 rounded-lg p-2"
+                        placeholder="Ivan.Ivanov@example.com">
+                </div>
+                <div class="flex flex-col gap-2">
+                    <label for="password">Пароль</label>
+                    <input id="password" type="password" v-model="request.password" class="border-2 rounded-lg p-2"
+                        placeholder="********">
+                </div>
+            </div>
+            <div>
+                <button type="submit"
+                    class="w-fit bg-transparent hover:bg-blue-500 active:bg-blue-800 font-bold py-2 px-4 rounded-lg text-blue-500 hover:text-white border-2 border-blue-500 hover:border-transparent mt-8">
+                    <span>Войти</span>
+                </button>
+            </div>
+        </form>
     </div>
 </template>
 
@@ -39,8 +37,8 @@
 import { ref } from 'vue';
 
 const request = ref({
-    firstName:'',
-    lastName:'',
+    firstName: '',
+    lastName: '',
     email: '',
     password: '',
 })
