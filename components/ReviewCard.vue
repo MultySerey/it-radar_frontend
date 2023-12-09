5cc1beeb545f4d4f68206c99afcad6764cd81f2b
Сергей Голованов <multyserey@yandex.ru>
2023-12-09 12:25:36 +0500

    review card template

---
+++ components/ReviewCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+const test_json = [
+    {
+        id: 0,
+        name: "GeekBox",
+        img_link: ""
+    },
+    {
+        id: 1,
+        name: "SkillBrains",
+        img_link: ""
+    },
+]
+</script>
+
+<style scoped>
+
+</style>
