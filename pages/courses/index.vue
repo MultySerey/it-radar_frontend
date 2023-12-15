8c98611aebc58b3adbc74c47eae70d2c00169901
MultySerey <multyserey@gmail.com>
2023-12-15 21:05:15 +0500

    FIXED AUTH

--- pages/courses/index.vue
+++ pages/courses/index.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="m-auto my-4 p-4 flex flex-wrap gap-4 w-fit">
         <div>
+            <div v-for="course in data">
+                {{ course }}
+            </div>
         </div>
     </div>
 </template>
@@ -8,17 +11,12 @@
 <script setup>
 const auth = useAuthStore()
 
-const accessCookie = useCookie('accessToken')
-accessCookie.value = auth.token.accessToken
-
 const { data } = await useFetch('http://localhost:5000/api/courses', {
     credentials: 'include',
     headers: {
-        Authorization: auth.token.accessToken
+        Authorization: `Bearer ${auth.token.accessToken}`
     },
 });
-
-console.log(auth.token.accessToken)
 </script>
 
 <style scoped></style>
