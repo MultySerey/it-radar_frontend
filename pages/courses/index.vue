e22edf1f6d64bba99226796bb63681cfece3665e
Сергей Голованов <multyserey@yandex.ru>
2023-12-15 15:48:15 +0500

    a

--- pages/courses/index.vue
+++ pages/courses/index.vue
@@ -1,24 +1,18 @@
 <template>
     <div class="m-auto my-4 p-4 flex flex-wrap gap-4 w-fit">
-        <div v-for="company in test_json">
-            <CompanyCard :company="company" />
+        <div>
         </div>
     </div>
 </template>
 
 <script setup>
-const test_json = [
-    {
-        id: 0,
-        name: "GeekBox",
-        img_link: ""
-    },
-    {
-        id: 1,
-        name: "SkillBrains",
-        img_link: ""
-    },
-]
+const auth = useAuthStore()
+
+const { data } = await useFetch('http://localhost:5000/api/courses', {
+    credentials: 'include'
+});
+
+console.log(data)
 </script>
 
 <style scoped></style>
